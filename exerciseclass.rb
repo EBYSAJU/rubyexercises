2bb9ce7268bd74b9c0c5ecd350a864dfd36a03d2
eby saju <ebysaju123@gmail.com>
2022-09-01 23:56:20 -0400

    class in ruby oop concepts

---
+++ exerciseclass.rb
@@ -0,0 +1,25 @@
+class Student
+    attr_accessor :last_name,:email
+    attr_reader :username
+    @first_name
+    @last_name
+    @email
+    @username
+    @password
+def first_name=(firstname)
+    @first_name=firstname
+end
+def first_name
+    @first_name
+end
+def to_s
+    "first name:#{@first_name}"
+end
+
+end
+eby=Student.new
+eby.first_name="eby"
+eby.last_name="saju"
+puts eby
+puts eby.first_name
+puts eby.last_name
